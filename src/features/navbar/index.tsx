a8aa75ca1b9e11c64aab0cb02cbad09687cff26e
startang141 <bintangadiyatma141@gmail.com>
2026-04-09 21:11:15 +0700

    navbar login and register

---
+++ src/features/navbar/index.tsx
@@ -0,0 +1,15 @@
+import NavImage from "./components/navImage";
+
+const Navbar = () => {
+  return (
+    <>
+      <div className="block shadow-md bg-white">
+        <div className="container mx-auto py-4">
+          <NavImage />
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Navbar;
