131bdd7a565c2ae8fad426003acee0af5aa12646
startang141 <bintangadiyatma141@gmail.com>
2026-04-18 01:15:55 +0700

    fix: button masuk

--- src/features/navbar/index.tsx
+++ src/features/navbar/index.tsx
@@ -4,7 +4,7 @@ import NavUser from "./components/navUser";
 const Navbar = () => {
   return (
     <>
-      <div className="relative shadow-md bg-white">
+      <div className="relative border-b border-gray-300 bg-white">
         <div className="container mx-auto px-5 md:px-0 py-4">
           <div className="flex flex-row justify-between w-full items-center">
             <NavImage />
